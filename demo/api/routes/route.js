55f78c771e309d0d14d2ab3a0a2f3fdd0f31ff31
hwb <hwb@313200064@qq.com>
2020-03-01 20:57:23 +0800

    测试接口

--- demo/api/routes/route.js
+++ demo/api/routes/route.js
@@ -5,7 +5,7 @@ const utility = require("utility");
 const jwt = require('jsonwebtoken');
 
 //引入配置文件
-const {tokenKey} = require('../public/js/config.js')
+const { tokenKey } = require('../public/js/config.js')
 //引入封装好的数据库中间件
 var databaseOp = require('../public/js/dbQuery.js');
 //引入封装好的返回结果的文件
@@ -14,47 +14,47 @@ var customRes = require('../public/js/customRes.js');
 var router = new Router();
 
 //根路径测试请求
-const index =  async ctx => {
+const index = async ctx => {
     databaseOp(`select * from user`).then(res => {
         console.log(res);
     });
     ctx.body = {
-        data:'Hello Koa!'
+        data: 'Hello Koa!'
     }
 }
 
 //注册请求
 const register = async ctx => {
     var resObj;
-    var {username,password} = ctx.request.body;
+    var { username, password } = ctx.request.body;
     username = username.trim();
-    console.log(username,password);
+    console.log(username, password);
     try {
         var queryRes = await databaseOp(`select * from userinfo where username = '${username}'`);
         console.log(queryRes);
-        if(queryRes.length){
+        if (queryRes.length) {
             //如果能查到数据，说明该用户名已经被占用，不可再注册
             console.log('该用户名已经被占用');
-            resObj = customRes(1,'该用户名已经被占用');
-        }else{
+            resObj = customRes(1, '该用户名已经被占用');
+        } else {
             //如果没查到数据，则进行注册操作
             console.log('用户名合法，开始注册');
             //将密码进行md5加密
             var password_md5 = utility.md5(password);
-            console.log(password,password_md5);
-            if(username === 'hwb'){
+            console.log(password, password_md5);
+            if (username === 'hwb') {
                 //超级管理员用户
                 var writeRes = await databaseOp(`insert into userinfo values ('${username}','${password_md5}',1,1)`);
-            }else{
+            } else {
                 //普通用户
                 var writeRes1 = await databaseOp(`insert into userinfo values ('${username}','${password_md5}',0,0)`);
             }
             console.log('写入数据库操作完成!');
-            resObj = customRes(1,'注册成功',username);
+            resObj = customRes(1, '注册成功', username);
         }
     } catch (error) {
         console.log(error);
-        resObj = customRes(0,'失败了');
+        resObj = customRes(0, '失败了');
     }
     ctx.response.type = 'json';
     ctx.response.body = resObj;
@@ -63,39 +63,39 @@ const register = async ctx => {
 //登录请求
 const login = async ctx => {
     var resObj;
-    var {username,password} = ctx.request.body;
+    var { username, password } = ctx.request.body;
     username = username.trim();
-    console.log(username,password);
+    console.log(username, password);
     try {
         //检查用户名是否存在
         var queryRes = await databaseOp(`select * from userinfo where username = '${username}'`);
         console.log(queryRes);
-        if(queryRes.length){
+        if (queryRes.length) {
             //如果用户名存在，检查密码是否正确
-            if(queryRes[0].password === utility.md5(password)){
+            if (queryRes[0].password === utility.md5(password)) {
                 //若正确，登录成功，返回token
                 console.log('登录成功');
                 const payload = {
                     username,
-                    isComment:queryRes[0].isComment,
-                    isEdit:queryRes[0].isEdit
+                    isComment: queryRes[0].isComment,
+                    isEdit: queryRes[0].isEdit
                 };
-                const token = jwt.sign(payload,tokenKey());
+                const token = jwt.sign(payload, tokenKey());
                 console.log(`这是生成的token:${token}`);
                 console.log(`这是token解析出来的:${JSON.stringify(jwt.decode(token))}`);
-                resObj = customRes(1,'登陆成功',token);
-            }else{
+                resObj = customRes(1, '登陆成功', token);
+            } else {
                 //密码错误
-                resObj = customRes(1,'输入的密码有误');
-            }    
-        }else{
+                resObj = customRes(1, '输入的密码有误');
+            }
+        } else {
             //没查到数据,用户名不存在
             console.log('用户名不存在');
-            resObj = customRes(1,'用户名不存在');
+            resObj = customRes(1, '用户名不存在');
         }
     } catch (error) {
         console.log(error);
-        resObj = customRes(0,'失败了');
+        resObj = customRes(0, '失败了');
     }
     ctx.response.type = 'json';
     ctx.response.body = resObj;
@@ -108,17 +108,61 @@ const test = async ctx => {
     console.log(userinfo);
     ctx.response.type = 'json';
     ctx.response.body = {
-        message:'test'
+        message: 'test'
     };
 }
 
-//路由菜单
-router.get('/',index );
-router.post('/register',register );
-router.post('/login',login );
-router.post('/test',test );
+//测试请求
+const getNavList = async ctx => {
+    const data = {
+        navList: [
+            {
+                title: '标题1',
+                id: 0
+            },
+            {
+                title: '标题2',
+                id: 1
+            },
+            {
+                title: '标题3',
+                id: 2
+            },
+            {
+                title: '标题4',
+                id: 3
+            },
+            {
+                title: '标题5',
+                id: 4
+            },
+            {
+                title: '标题6',
+                id: 5
+            },
+            {
+                title: '标题7',
+                id: 6
+            },
+            {
+                title: '标题8',
+                id: 7
+            }
+        ]
+    }
+    resObj = customRes(1, data);
+    ctx.response.type = 'json';
+    ctx.response.body = resObj;
+}
 
-module.exports = (app)=> {
+//路由菜单
+router.get('/', index);
+router.post('/register', register);
+router.post('/login', login);
+router.post('/test', test);
+router.get('/getNavList', getNavList);
+
+module.exports = (app) => {
     app.use(bodyParser())
         .use(router.routes())
         .use(router.allowedMethods())
