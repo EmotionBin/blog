3e7ca7b004d52eee0ab23c0280ee2ad9b010528c
huangweibin <313200064@qq.com>
2020-04-22 17:11:05 +0800

    feature:messagebox

--- demo/blog/src/views/menuList/MessageBoard.vue
+++ demo/blog/src/views/menuList/MessageBoard.vue
@@ -42,6 +42,15 @@
 				</div>
 			</div>
 		</div>
+		<div class="comment_input" :class="{'isEdit':isEdit}" @click="handleEdit">
+			<div class="arrow_wrap">
+				<div class="arrow_icon"></div>
+				<div class="arrow_mask"></div>
+			</div>
+			<div class="input_wrap">
+
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -51,8 +60,95 @@
 		components: {},
 		data() {
 			return {
+				//是否输入文本
+				isEdit:false,
+				//评论的数据
 				messageList:[
 					{
+						username:'hwb',
+						date:'这是一个时间戳',
+						floor:'1-0',
+						content:'测试',
+						comment:[
+							{
+								username:'test',
+								date:'这是一个时间戳',
+								floor:'1-1',
+								content:'测试回复1',
+								reply:''
+							},
+							{
+								username:'test1',
+								date:'这是一个时间戳',
+								floor:'1-2',
+								content:'测试回复2',
+								reply:'test'
+							}
+						]
+					},{
+						username:'hwb',
+						date:'这是一个时间戳',
+						floor:'1-0',
+						content:'测试',
+						comment:[
+							{
+								username:'test',
+								date:'这是一个时间戳',
+								floor:'1-1',
+								content:'测试回复1',
+								reply:''
+							},
+							{
+								username:'test1',
+								date:'这是一个时间戳',
+								floor:'1-2',
+								content:'测试回复2',
+								reply:'test'
+							}
+						]
+					},{
+						username:'hwb',
+						date:'这是一个时间戳',
+						floor:'1-0',
+						content:'测试',
+						comment:[
+							{
+								username:'test',
+								date:'这是一个时间戳',
+								floor:'1-1',
+								content:'测试回复1',
+								reply:''
+							},
+							{
+								username:'test1',
+								date:'这是一个时间戳',
+								floor:'1-2',
+								content:'测试回复2',
+								reply:'test'
+							}
+						]
+					},{
+						username:'hwb',
+						date:'这是一个时间戳',
+						floor:'1-0',
+						content:'测试',
+						comment:[
+							{
+								username:'test',
+								date:'这是一个时间戳',
+								floor:'1-1',
+								content:'测试回复1',
+								reply:''
+							},
+							{
+								username:'test1',
+								date:'这是一个时间戳',
+								floor:'1-2',
+								content:'测试回复2',
+								reply:'test'
+							}
+						]
+					},{
 						username:'hwb',
 						date:'这是一个时间戳',
 						floor:'1-0',
@@ -108,23 +204,27 @@
 
 		},
 		methods: {
-
+			handleEdit(){
+				const that = this;
+				that.isEdit = ! that.isEdit;
+			}
 		}
 
 	}
 </script>
 
 <style lang="scss">
+	$padding_value:20px;
 	.messageBoardCom{
 		width: 100%;
-		padding: 20px;
+		padding: $padding_value;
 		.message_wrap{
 			width: 100%;
 			.message_content{
 				display: flex;
 				flex-direction: column;
 				width: 100%;
-				padding: 4px 10px 20px 10px;
+				padding: 4px 10px $padding_value 10px;
 				.content_head{
 					display: flex;
 					width: 100%;
@@ -141,6 +241,7 @@
 					}
 				}
 				.content_body{
+					position: relative;
 					width: 100%;
 					.body_data{
 						display: -webkit-box;
@@ -148,15 +249,18 @@
 						-webkit-box-orient: vertical;
 						-webkit-line-clamp: 5;
 						width: 100%;
-						margin: 10px 0;
+						margin: $padding_value 16px;
 					}
 					.body_additional{
+						position: absolute;
+						right: 10px;
+						bottom: 2px;
 						display: flex;
 						justify-content: flex-end;
 						color: #6b6b6b;
     				font-size: 14px;
 						.additional_floor{
-							margin-right: 20px;
+							margin-right: $padding_value;
 						}
 					}
 				}
@@ -168,7 +272,7 @@
 					box-shadow: 0 0 2px #d1d1d1;
 					.foot_wrap{
 						width: 100%;
-						padding: 20px 10px;
+						padding: $padding_value 10px;
 						position: relative;
 						border-bottom: 1px solid #6b4a4a38;
 						&.isLastComment{
@@ -190,9 +294,56 @@
 						color: #6b6b6b;
     				font-size: 14px;
 						.additional_floor{
-							margin-right: 20px;
+							margin-right: $padding_value;
 						}
 					}
+				}
+			}
+		}
+		.comment_input{
+			position: fixed;
+			bottom: 0;
+			width: calc(60% - 2 * #{$padding_value});
+			padding: 10px;
+			transform: translateY(20px);
+			.arrow_wrap{
+				width: 100%;
+				display: flex;
+				justify-content: center;
+				.arrow_icon{
+					width: 60px;
+					height: 30px;
+					background-image: url('/images/arrow/arrow.svg');
+					background-position: center center;
+					background-size: 40%;
+					background-repeat: no-repeat;
+					background-color: #fff;
+					border-radius: 3px;
+					cursor: pointer;
+					@include commonShadow;
+				}
+				.arrow_mask{
+					position: absolute;
+					z-index: 300;
+					top: 36px;
+					left: 50%;
+					transform: translateX(-50%);
+					width: 60px;
+					height: 6px;
+					background-color: #fff;
+				}
+			}
+			.input_wrap{
+				width: 100%;
+				height: 300px;
+				background-color: #fff;
+				border-radius: 10px;
+				@include commonShadow;
+			}
+			&.isEdit{
+				transform: translateY(85%);
+				.arrow_icon{
+					transform:rotate(180deg);
 				}
 			}
 		}
